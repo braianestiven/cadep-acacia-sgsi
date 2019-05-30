04691e501eeed755645ab038f60ff6b8cb1c4bae
braian <estiven0109@gmail.com>
2019-05-29 21:56:30 -0500

    Subir archivos SGSI

---
+++ config/config_inc.php
@@ -0,0 +1,24 @@
+<?php
+$g_hostname               = 'localhost';
+$g_db_type                = 'mysqli';
+$g_database_name          = 'sgsi';
+$g_db_username            = 'root';
+$g_db_password            = '';
+
+$g_db_table_prefix          = 'sgsi';
+
+$g_default_timezone       = 'America/Bogota';
+
+$g_crypto_master_salt     = 'f66i6XLekXOPsd8L7/S66Q5Iw8n2nrV94JnPaQTXJ5Q=';
+
+$g_allow_signup = OFF;
+$g_enable_email_notification = ON;
+$g_phpMailer_method = PHPMAILER_METHOD_SMTP;
+$g_smtp_host='smtp.gmail.com';
+$g_smtp_connection_mode = 'tls';
+$g_smtp_port = 587;
+
+$g_smtp_username = 'estiven0109@gmail.com';
+$g_smtp_password = 'idontremember.1';
+
+?>
